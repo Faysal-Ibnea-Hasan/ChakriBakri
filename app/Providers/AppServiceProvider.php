d22a046cbe7cbbaa86f6609746e3e3a90bc1ac20
Jesan <foysal.jesan@gmail.com>
2024-06-26 13:41:20 +0600

    RedirectIfAuthenticated, Authenticated routes,Guest routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RedirectIfAuthenticated::redirectUsing(fn($request)=>route('account.profile'));
+        Authenticate::redirectUsing(fn($request)=>route('account.login'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,26 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/account/registration',[UserController::class,'userRegistration'])->name('account.reg');
-Route::post('/account/registration/process',[UserController::class,'userRegistrationProcess'])->name('account.reg.process');
-Route::get('/account/login',[UserController::class,'userLogin'])->name('account.login');
-Route::post('/account/auth',[UserController::class,'userAuth'])->name('account.auth');
-Route::get('/account/profile',[UserController::class,'userProfile'])->name('account.profile');
-Route::get('/account/logout',[UserController::class,'userLogout'])->name('account.logout');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+
+
+
+Route::group(['account'], function () {
+    //Guest routes
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('/account/registration', [UserController::class, 'userRegistration'])->name('account.reg');
+        Route::post('/account/registration/process', [UserController::class, 'userRegistrationProcess'])->name('account.reg.process');
+        Route::get('/account/login', [UserController::class, 'userLogin'])->name('account.login');
+        Route::post('/account/auth', [UserController::class, 'userAuth'])->name('account.auth');
+    });
+
+    //Auth routes
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('/account/profile', [UserController::class, 'userProfile'])->name('account.profile');
+        Route::get('/account/logout', [UserController::class, 'userLogout'])->name('account.logout');
+    });
+});
