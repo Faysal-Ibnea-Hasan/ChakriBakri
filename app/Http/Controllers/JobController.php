df13c8f3e43a2441a91687d890f768dd265a8805
Jesan <foysal.jesan@gmail.com>
2024-07-10 22:41:45 +0600

    minor changes

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -10,14 +10,15 @@ use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         // Retrieve the active categories sorted by name in ascending order
         $catagories = Catagory::where('status', 1)->orderBy('name', 'ASC')->get();
         // Retrieve the active jobTypes sorted by name in ascending order
         $jobTypes = JobType::where('status', 1)->orderBy('name', 'ASC')->get();
         // Retrieve the  latest jobs with their job types, sorted by creation date in descending order
         $letestJobs = Job::where('status', 1)->with('jobType')->orderBy('created_at', 'DESC')->paginate(9);
-        return view('front.job_s.all_job',[
+        return view('front.job_s.all_job', [
             'catagories' => $catagories,
             'jobTypes' => $jobTypes,
             'letestJobs' => $letestJobs
